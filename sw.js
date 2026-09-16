e0ca79e4651649d520a8c09dc7e8ccad812d8179
hldfortes-beep <hldfortes@gmail.com>
2026-09-16 22:11:48 -0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='future-blue-shark-v4-5';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
